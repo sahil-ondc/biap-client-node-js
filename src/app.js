44dc8325618bc738768f981b68c6e585911e2be4
jsshubham <shubham.joshi@thewitslab.com>
2024-03-18 17:41:56 +0530

    fix: CORS Issue

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(
 app.use(logger("combined"));
 
 const corsOptionsDelegate = function (req, callback) {
-  let corsOptions = { credentials: true };
+  let corsOptions = { credentials: false };
   corsOptions["origin"] = true;
   callback(null, corsOptions);
 };
